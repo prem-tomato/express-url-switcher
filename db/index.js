5787ed6a3e68dd2ab4fe33b33490c550651c5050
prem.tomato12 <prem.tomato12@gmail.com>
2025-08-12 16:31:14 +0530

    fix connection

--- db/index.js
+++ db/index.js
@@ -3,18 +3,26 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  // optional: you can add ssl config for production
-});
+// Reuse pool in development & serverless
+let pool;
 
-pool.on("error", (err) => {
-  console.error("Unexpected error on idle client", err);
-  process.exit(-1);
-});
+if (!global.pgPool) {
+  global.pgPool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    max: 1, // limit for serverless
+    idleTimeoutMillis: 30000
+  });
+
+  global.pgPool.on("error", (err) => {
+    console.error("Unexpected error on idle client", err);
+    process.exit(-1);
+  });
+}
+
+pool = global.pgPool;
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
